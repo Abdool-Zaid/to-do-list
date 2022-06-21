09d29958724fef12b246ac5d7f2facf6cf0020ca
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-06-21 20:56:51 +0200

    added array

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("active")
+const tasks=[
+{name:"Yes, yes! I can do it! I'll fix my life's clock!",
+description:"My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox"
+},
+{
+    name:"shower",
+    description:"use soap"
+}
+]
+
+
+document.querySelector("#target").innerHTML = ""
+tasks.forEach((task) => {
+    document.querySelector("#target").innerHTML += `
+    <div class="taskItem">
+    <h3>${task.name}</h3>
+    <p>${task.description}</p>
+</div>
+    `; 
+})
