38f2971893cb89be9c162e816339dd67de9edc08
unknown <aztoorabally7447@gmail.com>
2022-06-22 09:00:17 +0200

    adding task functions'

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ const tasks=[
 {name:"Yes, yes! I can do it! I'll fix my life's clock!",
 description:"My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox"
 },
-{
-    name:"shower",
-    description:"use soap"
-}
+
 ]
 
 
-document.querySelector("#target").innerHTML = ""
+ display =() =>{document.querySelector("#target").innerHTML = ""
 tasks.forEach((task) => {
     document.querySelector("#target").innerHTML += `
     <div class="taskItem">
@@ -18,4 +15,17 @@ tasks.forEach((task) => {
     <p>${task.description}</p>
 </div>
     `; 
-})
+})}
+display()
+addToTasks = () =>{
+    let TaskName= document.querySelector("#taskInput").value
+    let TaskDescription = document.querySelector("#taskDescription").value
+    tasks.push({name:TaskName,
+        description:TaskDescription},)
+        console.log("entry successful")
+        display()
+    
+}
+searchTasks=() =>{
+    
+}
