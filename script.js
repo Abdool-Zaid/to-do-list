1e06402b697c8ef8da739c21aa8d090f986caf4d
unknown <aztoorabally7447@gmail.com>
2022-06-22 12:21:13 +0200

    update

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-console.log("active")
-const tasks=[
-{name:"Yes, yes! I can do it! I'll fix my life's clock!",
-description:"My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox"
-},
+console.log("active");
+let today=new Date().toLocaleString(undefined, {
+    month: "numeric", day: "numeric", 
+});
+const tasks = [
+  {
+    id: 1,
+    name: "Yes, yes! I can do it! I'll fix my life's clock!",
+    description:
+      "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
+      createdDate: today,
+      status: "need to do",
+  },
+];
 
-]
-
-
- display =() =>{document.querySelector("#target").innerHTML = ""
-tasks.forEach((task) => {
-    document.querySelector("#target").innerHTML += `
-    <div class="taskItem">
-    <h3>${task.name}</h3>
-    <p>${task.description}</p>
-</div>
-    `; 
-})}
-display()
-addToTasks = () =>{
-    let TaskName= document.querySelector("#taskInput").value
-    let TaskDescription = document.querySelector("#taskDescription").value
-    tasks.push({name:TaskName,
-        description:TaskDescription},)
-        console.log("entry successful")
-        display()
-        
+display = () => {
+  document.querySelector("#target").innerHTML = "";
+  tasks.forEach((task) => {
+      document.querySelector("#target").innerHTML += `
+      <div class="taskItem">
+      <p>${task.id}</p>
+      <h3>${task.name}</h3>
+      <p>${task.description}</p>
+      <p dir="rtl" class="subInfo">${task.createdDate}</p>
+      <p dir="rtl" class="subInfo">${task.status}</p>
+      </div>
+      `;
+    });
+};
+display();
+addToTasks = () => {
+    let TaskStatus = document.querySelector("#TaskStatus").value;
+    let TaskName = document.querySelector("#taskInput").value;
+    let TaskDescription = document.querySelector("#taskDescription").value;
+    if(TaskName==""||TaskStatus==""){
+    alert("blank entry detected")
     }
-    searchTasks=() =>{
-        let FindTask= document.querySelector("#SecondR").value
-       let SearchResult= tasks.find( tasks=> tasks.name===FindTask); 
+    else{
+      tasks.push({ id:tasks.length+1 ,name: TaskName, description: TaskDescription, createdDate: today, status:TaskStatus  });
+      console.log("entry successful");
+    }
+  display();
+};
+searchTasks = () => {
+  let FindTask = document.querySelector("#SecondR").value;
+  let SearchResult = tasks.find((tasks) => tasks.name === FindTask);
 
-     console.log(SearchResult)
-}
+  console.log(SearchResult);
+};
