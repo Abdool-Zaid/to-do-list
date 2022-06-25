fb1db59c2d732d8089491d819baf04e70adfbd44
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-06-25 16:32:04 +0200

    update

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 console.log("active");
-
-// const tasks =  [
-  //   {
-    //     id: 1,
-    //     name: "Yes, yes! I can do it! I'll fix my life's clock!",
-    //     description:
-    //       "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
-    //     createdDate: today,
-//     status: "need to do",
-//   },
-// ];
-
+// create
 const tasks = JSON.parse(localStorage.getItem("tasks"))
 ? JSON.parse(localStorage.getItem("tasks"))
 : [
@@ -23,15 +12,57 @@ const tasks = JSON.parse(localStorage.getItem("tasks"))
   },
 ];
 
+
 resetTasks = () => {
   localStorage.setItem("tasks", JSON.stringify(tasks));
   window.location.reload();
 };
 
-display = () => {
-      let retrievedTask = localStorage.getItem("tasks");
-      document.querySelector("#target").innerHTML = "";
-      JSON.parse(retrievedTask).forEach((task) => {
+addToTasks = () => {
+    let TaskStatus = document.querySelector("#TaskStatus").value;
+    let TaskName = document.querySelector("#taskInput").value;
+    let TaskDescription = document.querySelector("#taskDescription").value;
+    if (TaskName == "") {
+      alert("blank entry detected");
+    } else {
+      tasks.push({
+        id: tasks.length + 1,
+        name: TaskName,
+        description: TaskDescription,
+        status: TaskStatus,
+      });
+      
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      
+      console.log("entry successful")
+    }
+  }
+  
+  
+  // read all
+  // sort by status
+  display = () => {
+    document.querySelector("#target").innerHTML = "";
+    
+    // display
+    // if (listed tasks.staus = "selected status"?display from index 1:display index o)
+    // let RetrievedTaskArray= JSON.parse(localStorage.getItem("tasks"))
+    // let selectionStatus = document.querySelector("#selectType").value;
+    // for (let i = 0; i < RetrievedTaskArray.length; i++) {
+    //   // 
+    //   if (selectionStatus =RetrievedTaskArray.status){
+
+    //     document.querySelector("#target").innerHTML += `
+    //     <div class="selectedtasks"   >
+    //     <p>${tasks.id}</p>
+    //     <h3 id="h3">${tasks.name}</h3>
+    //     <p>${tasks.description}</p>
+    //     ` 
+    //   } 
+      // else{
+
+        let retrievedTask = localStorage.getItem("tasks");
+        JSON.parse(retrievedTask).forEach((task) => {
         document.querySelector("#target").innerHTML += `
         <div class="taskItem"   >
         <p>${task.id}</p>
@@ -41,88 +72,111 @@ display = () => {
         <p>${task.description}</p>
         </label>
         <p dir="rtl" class="subInfo">${task.status}</p>
-        
         </div>
         `;
-        document.querySelector('.taskItem').addEventListener("mouseover",function (){
-          document.querySelector('#deleteitem').innerHTML=` <button type="button" onclick="deleteItem()" id="${task.id}">delete</button>`});
-          document.querySelector('.taskItem').addEventListener("mouseout",function (){
-            document.querySelector('#deleteitem').innerHTML=` `});
-          });
-        };
-        display();  
-        addToTasks = () => {
-          let TaskStatus = document.querySelector("#TaskStatus").value;
-          let TaskName = document.querySelector("#taskInput").value;
-          let TaskDescription = document.querySelector("#taskDescription").value;
-          if (TaskName == "") {
-            alert("blank entry detected");
-          } else {
-            tasks.push({
-              id: tasks.length + 1,
-              name: TaskName,
-              description: TaskDescription,
-              status: TaskStatus,
-            });
-            localStorage.getItem(tasks)
-            ? console.log("in memory")
-            : localStorage.setItem("tasks", JSON.stringify(tasks));
-            
-            console.log("entry successful");
-          }
-          
-          display();
-        };
-        clearTasks = () => {
-          localStorage.clear();
-          console.log(localStorage);
-          resetTasks();
-        };
-        searchTasks = () => {
-          let FindTask = document.querySelector("#SecondR").value;
-          let SearchResult = tasks.find((tasks) => tasks.name === FindTask);
-          console.log(SearchResult);
-          document.querySelector("#target").innerHTML = "";
-          document.querySelector("#target").innerHTML = `
-          <div class="searchItem" >
-          <p>${SearchResult.id}</p>
-          <h3>${SearchResult.name}</h3>
-          <p>${SearchResult.description}</p>
-          <p dir="rtl" class="subInfo">${SearchResult.status}</p>
-          <button onclick="deleteItem()">delete task</button>
-          
-          
-          </div>
-          
-          `;
-        };
         
-        editTasks = () => {
-          alert("alert");
-        };
-        
-        reviewTask = (caller) => {
-          // use search function to select task
-          let target = caller.id;
-          console.log("area element id = " + target);
-        };
-        
-        // document.querySelector('.taskItem').addEventListener("dblclick",function (){ console.log(tasks.length)});
-        
-        // deleteTask = () => {
-          //   tasks.splice(id, 1);
-          //   localStorage.setItem("tasks", JSON.stringify(tasks));
-          //   display();
-          //   console.log("read =true");
-          // };
-          deleteItem=()=>{
-            let rawText =document.querySelector('.searchItem').textContent;
-            let id =JSON.parse(rawText.split(' ').slice(10,11))-1
-            
-            console.log(id)
-            tasks.splice([], id);
-            console.log(tasks.length);
-            window.location.reload();
-            console.log("removal=complete")
-          }
+      });
+      
+    // }
+    // };
+  }
+  display();
+
+// read one
+
+
+
+// update  
+
+
+// update all
+// remove completed tasks 
+
+
+
+
+// delete
+// delted all selected(sorted)
+
+
+// delete last
+
+// delete first
+
+
+
+
+
+
+
+
+
+
+
+
+// addToTasks = () => {
+//   let TaskStatus = document.querySelector("#TaskStatus").value;
+//   let TaskName = document.querySelector("#taskInput").value;
+//   let TaskDescription = document.querySelector("#taskDescription").value;
+//   if (TaskName == "") {
+//     alert("blank entry detected");
+//   } else {
+//     tasks.push({
+//       id: tasks.length + 1,
+//       name: TaskName,
+//       description: TaskDescription,
+//       status: TaskStatus,
+//     });
+//     localStorage.getItem(tasks)
+//       ? console.log("in memory")
+//       : localStorage.setItem("tasks", JSON.stringify(tasks));
+
+//     console.log("entry successful");
+//   }
+
+//   display();
+// };
+// clearTasks = () => {
+//   localStorage.length=0;
+//   console.log(localStorage);
+//   resetTasks();
+// };
+// searchTasks = () => {
+//   let FindTask = document.querySelector("#SecondR").value.toLowerCase();
+//   let SearchResult = tasks.find((tasks) => tasks.name === FindTask);
+//   console.log(SearchResult);
+//   document.querySelector("#target").innerHTML = "";
+//   document.querySelector("#target").innerHTML = `
+//           <div class="searchItem" >
+//           <p>${SearchResult.id}</p>
+//           <h3>${SearchResult.name}</h3>
+//           <p>${SearchResult.description}</p>
+//           <p dir="rtl" class="subInfo">${SearchResult.status}</p>
+//           <button onclick="deleteItem()">delete task</button>
           
+          
+//           </div>
+          
+//           `;
+// };
+
+// editTasks = () => {
+//   alert("alert");
+// };
+
+// reviewTask = (caller) => {
+//   // use search function to select task
+//   let target = caller.id;
+//   console.log("area element id = " + target);
+// };
+
+
+// deleteItem = () => {
+//   let rawText = document.querySelector(".searchItem").textContent;
+//   let id = JSON.parse(rawText.split(" ").slice(10, 11)) - 1;
+//   console.log(id);
+//   localStorage.removeItem(tasks[id]);
+//   console.log(tasks);
+//   // window.location.reload();
+//   console.log("removal=complete");
+// };
