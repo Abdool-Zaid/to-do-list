6d5854c897741c211e50a0df935a9aa4b0c6bec3
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-06-25 18:07:25 +0200

    delete last item

--- script.js
+++ script.js
@@ -1,69 +1,68 @@
 console.log("active");
 // create
-const tasks = JSON.parse(localStorage.getItem("tasks"))
-  ? JSON.parse(localStorage.getItem("tasks"))
+const list = JSON.parse(localStorage.getItem("list"))
+  ? JSON.parse(localStorage.getItem("list"))
   : [
-    {
-      id: 1,
-      name: "Yes, yes! I can do it! I'll fix my life's clock!",
-      description:
-      "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
-      status: "need to do",
-    },
-  ];
-
-  resetTasks = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    window.location.reload();
-  };
-  
-  addToTasks = () => {
+      {
+        id: 1,
+        name: "Yes, yes! I can do it! I'll fix my life's clock!",
+        description:
+        "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
+        status: "need to do",
+      },
+    ];
+    
+    resetTasks = () => {
+      localStorage.setItem("list", JSON.stringify(list));
+      window.location.reload();
+    };
+    
+    addToTasks = () => {
   let TaskStatus = document.querySelector("#TaskStatus").value;
   let TaskName = document.querySelector("#taskInput").value;
   let TaskDescription = document.querySelector("#taskDescription").value;
   if (TaskName == "") {
     alert("blank entry detected");
   } else {
-    tasks.push({
-      id: tasks.length + 1,
+    list.push({
+      id: list.length + 1,
       name: TaskName,
       description: TaskDescription,
       status: TaskStatus,
     });
-    
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    localStorage.setItem("list", JSON.stringify(list));
     
     window.location.reload();
     console.log("entry successful");
   }
 };
 
-
 // read all
 // sort by status
 display = () => {
   document.querySelector("#target").innerHTML = "";
-  let RetrievedTaskArray = JSON.parse(localStorage.getItem("tasks"));
+  let RetrievedTaskArray = JSON.parse(localStorage.getItem("list"));
   
   // display
-  // if (listed tasks.staus = "selected status"?display from index 1:display index o)
+  // if (listed list.status = "selected status"?display from index 1:display index o)
   // let selectionStatus = document.querySelector("#selectType").value;
   // for (let i = 0; i < RetrievedTaskArray.length; i++) {
     //   //
-    //   if (selectionStatus =RetrievedTaskArray.status){
-      
-      //     document.querySelector("#target").innerHTML += `
-      //     <div class="selectedtasks"   >
-      //     <p>${tasks.id}</p>
-      //     <h3 id="h3">${tasks.name}</h3>
-      //     <p>${tasks.description}</p>
-      //     `
-      //   }
-      // else{
-        
-        let retrievedTask = localStorage.getItem("tasks");
-        JSON.parse(retrievedTask).forEach((task) => {
-          document.querySelector("#target").innerHTML += `
+  //   if (selectionStatus =RetrievedTaskArray.status){
+
+  //     document.querySelector("#target").innerHTML += `
+  //     <div class="selectedtasks"   >
+  //     <p>${list.id}</p>
+  //     <h3 id="h3">${list.name}</h3>
+  //     <p>${list.description}</p>
+  //     `
+  //   }
+  // else{
+
+    let retrievedTask = localStorage.getItem("list");
+    JSON.parse(retrievedTask).forEach((task) => {
+      document.querySelector("#target").innerHTML += `
         <div class="taskItem"   >
         <p>${task.id}</p>
         <input type="checkbox" name="checkbox" class="checkbox">
@@ -74,11 +73,11 @@ display = () => {
         <p dir="rtl" class="subInfo">${task.status}</p>
         </div>
         `;
-  });
-
-  // }
-  // };
-};
+      });
+      
+      // }
+      // };
+    };
 display();
 
 // read one
@@ -86,53 +85,45 @@ display();
 // update
 
 // update all
-// remove completed tasks
-
-// delete
-// deleted all selected(sorted)
+// remove completed list
 
 // delete last
 RemoveFinal = () => {
-  JSON.parse(localStorage.getItem('tasks'))
-  console.log(tasks);
-  let NewTasks = tasks.pop();
-  console.log(NewTasks);
+  JSON.parse(localStorage.getItem("list"));
+  console.log(list);
+  list.pop()
+  let Newlist = list; 
+  console.log(Newlist);
   
-  localStorage.setItem(tasks,JSON.stringify(NewTasks));
-  console.log("removed last item");
-  display();
-  // window.location.reload();
+  localStorage.setItem("list", JSON.stringify(Newlist));
+  console.log(list);
+  
+  document.querySelector("#target").innerHTML = "";
+console.log("removed last item");
+display();
+// window.location.reload();
 };
-
-
-
-
+// delete
+// deleted all selected(sorted)
 
 // delete first
 
-
-
-
-
-
-
-
 // addToTasks = () => {
-//   let TaskStatus = document.querySelector("#TaskStatus").value;
-//   let TaskName = document.querySelector("#taskInput").value;
-//   let TaskDescription = document.querySelector("#taskDescription").value;
-//   if (TaskName == "") {
-//     alert("blank entry detected");
-//   } else {
-//     tasks.push({
-//       id: tasks.length + 1,
+  //   let TaskStatus = document.querySelector("#TaskStatus").value;
+  //   let TaskName = document.querySelector("#taskInput").value;
+  //   let TaskDescription = document.querySelector("#taskDescription").value;
+  //   if (TaskName == "") {
+    //     alert("blank entry detected");
+    //   } else {
+//     list.push({
+  //       id: list.length + 1,
 //       name: TaskName,
 //       description: TaskDescription,
 //       status: TaskStatus,
 //     });
-//     localStorage.getItem(tasks)
+//     localStorage.getItem(list)
 //       ? console.log("in memory")
-//       : localStorage.setItem("tasks", JSON.stringify(tasks));
+//       : localStorage.setItem("list", JSON.stringify(list));
 
 //     console.log("entry successful");
 //   }
@@ -146,7 +137,7 @@ RemoveFinal = () => {
 // };
 // searchTasks = () => {
 //   let FindTask = document.querySelector("#SecondR").value.toLowerCase();
-//   let SearchResult = tasks.find((tasks) => tasks.name === FindTask);
+//   let SearchResult = list.find((list) => list.name === FindTask);
 //   console.log(SearchResult);
 //   document.querySelector("#target").innerHTML = "";
 //   document.querySelector("#target").innerHTML = `
@@ -176,8 +167,8 @@ RemoveFinal = () => {
 //   let rawText = document.querySelector(".searchItem").textContent;
 //   let id = JSON.parse(rawText.split(" ").slice(10, 11)) - 1;
 //   console.log(id);
-//   localStorage.removeItem(tasks[id]);
-//   console.log(tasks);
+//   localStorage.removeItem(list[id]);
+//   console.log(list);
 //   // window.location.reload();
 //   console.log("removal=complete");
 // };
