79db4f5717fb9a93014eeb3b255665a990cd0239
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-06-25 17:22:29 +0200

    update

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
 console.log("active");
 // create
 const tasks = JSON.parse(localStorage.getItem("tasks"))
-? JSON.parse(localStorage.getItem("tasks"))
-: [
-  {
-    id: 1,
-    name: "Yes, yes! I can do it! I'll fix my life's clock!",
-    description:
-    "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
-    status: "need to do",
-  },
-];
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [
+    {
+      id: 1,
+      name: "Yes, yes! I can do it! I'll fix my life's clock!",
+      description:
+      "My children called me dad. My wife call me Milien but from now on the world will come to know me as.... Nox",
+      status: "need to do",
+    },
+  ];
 
-
-resetTasks = () => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  window.location.reload();
+  resetTasks = () => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    window.location.reload();
+  };
+  
+  addToTasks = () => {
+  let TaskStatus = document.querySelector("#TaskStatus").value;
+  let TaskName = document.querySelector("#taskInput").value;
+  let TaskDescription = document.querySelector("#taskDescription").value;
+  if (TaskName == "") {
+    alert("blank entry detected");
+  } else {
+    tasks.push({
+      id: tasks.length + 1,
+      name: TaskName,
+      description: TaskDescription,
+      status: TaskStatus,
+    });
+    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    
+    window.location.reload();
+    console.log("entry successful");
+  }
 };
 
-addToTasks = () => {
-    let TaskStatus = document.querySelector("#TaskStatus").value;
-    let TaskName = document.querySelector("#taskInput").value;
-    let TaskDescription = document.querySelector("#taskDescription").value;
-    if (TaskName == "") {
-      alert("blank entry detected");
-    } else {
-      tasks.push({
-        id: tasks.length + 1,
-        name: TaskName,
-        description: TaskDescription,
-        status: TaskStatus,
-      });
-      
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-      
-      console.log("entry successful")
-    }
-  }
+
+// read all
+// sort by status
+display = () => {
+  document.querySelector("#target").innerHTML = "";
+  let RetrievedTaskArray = JSON.parse(localStorage.getItem("tasks"));
   
-  
-  // read all
-  // sort by status
-  display = () => {
-    document.querySelector("#target").innerHTML = "";
-    
-    // display
-    // if (listed tasks.staus = "selected status"?display from index 1:display index o)
-    // let RetrievedTaskArray= JSON.parse(localStorage.getItem("tasks"))
-    // let selectionStatus = document.querySelector("#selectType").value;
-    // for (let i = 0; i < RetrievedTaskArray.length; i++) {
-    //   // 
+  // display
+  // if (listed tasks.staus = "selected status"?display from index 1:display index o)
+  // let selectionStatus = document.querySelector("#selectType").value;
+  // for (let i = 0; i < RetrievedTaskArray.length; i++) {
+    //   //
     //   if (selectionStatus =RetrievedTaskArray.status){
-
-    //     document.querySelector("#target").innerHTML += `
-    //     <div class="selectedtasks"   >
-    //     <p>${tasks.id}</p>
-    //     <h3 id="h3">${tasks.name}</h3>
-    //     <p>${tasks.description}</p>
-    //     ` 
-    //   } 
+      
+      //     document.querySelector("#target").innerHTML += `
+      //     <div class="selectedtasks"   >
+      //     <p>${tasks.id}</p>
+      //     <h3 id="h3">${tasks.name}</h3>
+      //     <p>${tasks.description}</p>
+      //     `
+      //   }
       // else{
-
+        
         let retrievedTask = localStorage.getItem("tasks");
         JSON.parse(retrievedTask).forEach((task) => {
-        document.querySelector("#target").innerHTML += `
+          document.querySelector("#target").innerHTML += `
         <div class="taskItem"   >
         <p>${task.id}</p>
         <input type="checkbox" name="checkbox" class="checkbox">
@@ -74,38 +74,41 @@ addToTasks = () => {
         <p dir="rtl" class="subInfo">${task.status}</p>
         </div>
         `;
-        
-      });
-      
-    // }
-    // };
-  }
-  display();
+  });
+
+  // }
+  // };
+};
+display();
 
 // read one
 
-
-
-// update  
-
+// update
 
 // update all
-// remove completed tasks 
-
-
-
+// remove completed tasks
 
 // delete
-// delted all selected(sorted)
-
+// deleted all selected(sorted)
 
 // delete last
+RemoveFinal = () => {
+  JSON.parse(localStorage.getItem('tasks'))
+  console.log(tasks);
+  let NewTasks = tasks.pop();
+  console.log(NewTasks);
+  
+  localStorage.setItem(tasks,JSON.stringify(NewTasks));
+  console.log("removed last item");
+  display();
+  // window.location.reload();
+};
+
+
+
+
 
 // delete first
-
-
-
-
 
 
 
@@ -153,10 +156,9 @@ addToTasks = () => {
 //           <p>${SearchResult.description}</p>
 //           <p dir="rtl" class="subInfo">${SearchResult.status}</p>
 //           <button onclick="deleteItem()">delete task</button>
-          
-          
+
 //           </div>
-          
+
 //           `;
 // };
 
@@ -169,7 +171,6 @@ addToTasks = () => {
 //   let target = caller.id;
 //   console.log("area element id = " + target);
 // };
-
 
 // deleteItem = () => {
 //   let rawText = document.querySelector(".searchItem").textContent;
