a24b8f8059a17e730b710bd3a5ef94e5a846cf0a
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-06-25 23:07:29 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("active");
-
 const list = JSON.parse(localStorage.getItem("list"))
   ? JSON.parse(localStorage.getItem("list"))
   : [
@@ -11,12 +9,10 @@ const list = JSON.parse(localStorage.getItem("list"))
         status: "need to do",
       },
     ];
-
 resetTasks = () => {
   localStorage.setItem("list", JSON.stringify(list));
   window.location.reload();
 };
-
 addToTasks = () => {
   let TaskStatus = document.querySelector("#TaskStatus").value;
   let TaskName = document.querySelector("#taskInput").value;
@@ -30,41 +26,15 @@ addToTasks = () => {
       description: TaskDescription,
       status: TaskStatus,
     });
-
     localStorage.setItem("list", JSON.stringify(list));
-
     window.location.reload();
-    console.log("entry successful");
   }
 };
-
 // read all
 // sort by status
-
-
-
-
-
 display = () => {
   document.querySelector("#target").innerHTML = "";
   let RetrievedTaskArray = JSON.parse(localStorage.getItem("list"));
-
-  // display
-  // if (listed list.status = "selected status"?display from index 1:display index o)
-  // let selectionStatus = document.querySelector("#selectType").value;
-  // for (let i = 0; i < RetrievedTaskArray.length; i++) {
-  //   //
-  //   if (selectionStatus =RetrievedTaskArray.status){
-
-  //     document.querySelector("#target").innerHTML += `
-  //     <div class="selectedtasks"   >
-  //     <p>${list.id}</p>
-  //     <h3 id="h3">${list.name}</h3>
-  //     <p>${list.description}</p>
-  //     `
-  //   }
-  // else{
-
   let retrievedTask = localStorage.getItem("list");
   JSON.parse(retrievedTask).forEach((task) => {
     document.querySelector("#target").innerHTML += `
@@ -79,15 +49,11 @@ display = () => {
     </div>
     `;
   });
-
 };
 display();
-
-
 searchTasks = () => {
   let FindTask = document.querySelector("#SecondR").value.toLowerCase();
   let SearchResult = list.find((list) => list.name === FindTask);
-
   document.querySelector("#target").innerHTML = "";
   document.querySelector("#target").innerHTML = `
   <div class="searchItem" >
@@ -100,20 +66,22 @@ searchTasks = () => {
           <p dir="rtl" class="subInfo">${SearchResult.status}</p>
           <button onclick="editItem()">edit</button>
           <button onclick="deleteOne()">delete</button>
-
           </div>
-
           `;
-  
-        sessionStorage.setItem('itemToBeEdited', JSON.stringify({'index':SearchResult.id-1,'name':SearchResult.name, 'description':SearchResult.description}));
-        };
-        
-        // update
-        editItem=()=>{
-          let values= JSON.parse(sessionStorage.getItem('itemToBeEdited'))
-  
-          document.querySelector(".searchItem").innerHTML = "";
-          document.querySelector(".searchItem").innerHTML = `
+  sessionStorage.setItem(
+    "itemToBeEdited",
+    JSON.stringify({
+      index: SearchResult.id - 1,
+      name: SearchResult.name,
+      description: SearchResult.description,
+    })
+  );
+};
+// update
+editItem = () => {
+  let values = JSON.parse(sessionStorage.getItem("itemToBeEdited"));
+  document.querySelector(".searchItem").innerHTML = "";
+  document.querySelector(".searchItem").innerHTML = `
           <div id="editItem">
           <form onsubmit="event.preventDefault()">
           <input type="text" id="subjectName" value="${values.name}" />
@@ -127,144 +95,51 @@ searchTasks = () => {
           </form>
           </div>
           `;
-
-          updateTask=()=>{
-             JSON.parse(localStorage.getItem("list"));
-            let target = list[values.index]
-            let name= document.querySelector('#subjectName').value
-            let description= document.querySelector('#taskDescription').value
-            let status= document.querySelector('#TaskStatus').value
-        
-          target.name = name;
-          target.description= description;
-          target.status=status;
-          console.log(list)
-          localStorage.setItem('list',JSON.stringify(list))
-            console.log('updated')
-            window.location.reload();
-          }
-          
-        
-          
-          
-          console.log('editted')
-          
-        }
-        
-
-
+  updateTask = () => {
+    JSON.parse(localStorage.getItem("list"));
+    let target = list[values.index];
+    let name = document.querySelector("#subjectName").value;
+    let description = document.querySelector("#taskDescription").value;
+    let status = document.querySelector("#TaskStatus").value;
+    target.name = name;
+    target.description = description;
+    target.status = status;
+    localStorage.setItem("list", JSON.stringify(list));
+    window.location.reload();
+  };
+};
 // update all
-// remove completed list
-
-
+// remove completed lis
 RemoveFinal = () => {
   JSON.parse(localStorage.getItem("list"));
-  console.log(list);
   list.pop();
   let Newlist = list;
-  console.log(Newlist);
-
   localStorage.setItem("list", JSON.stringify(Newlist));
-  console.log(list);
-
   document.querySelector("#target").innerHTML = "";
-  console.log("removed last item");
   display();
 };
-
-
-
-deleteOne=()=>{
+deleteOne = () => {
   JSON.parse(localStorage.getItem("list"));
-  console.log(list);
-  list.splice((document.querySelector('.id').innerText-1),1);
+  list.splice(document.querySelector(".id").innerText - 1, 1);
   let Newlist = list;
-  console.log(Newlist);
-
   localStorage.setItem("list", JSON.stringify(Newlist));
-  console.log(list);
-
   document.querySelector("#target").innerHTML = "";
-  console.log("removed one item");
   display();
-}
-
-
-DeleteFirstTask= () => {
+};
+DeleteFirstTask = () => {
   JSON.parse(localStorage.getItem("list"));
-  console.log(list);
   list.shift();
   let Newlist = list;
-  console.log(Newlist);
-
   localStorage.setItem("list", JSON.stringify(Newlist));
-  console.log(list);
-
   document.querySelector("#target").innerHTML = "";
-  console.log("removed first item");
   display();
 };
-
-
-// delete all
-
-RemoveAll=()=>{
+RemoveAll = () => {
   let RetrievedTaskArray = JSON.parse(localStorage.getItem("list"));
-
   for (let i = 0; i <= RetrievedTaskArray.length; i++) {
-RetrievedTaskArray.pop()
-let clear =RetrievedTaskArray
-localStorage.setItem("list", JSON.stringify(clear));
-console.log(clear);
-window.location.reload();
-    }
-
-}
-
-// addToTasks = () => {
-//   let TaskStatus = document.querySelector("#TaskStatus").value;
-//   let TaskName = document.querySelector("#taskInput").value;
-//   let TaskDescription = document.querySelector("#taskDescription").value;
-//   if (TaskName == "") {
-//     alert("blank entry detected");
-//   } else {
-//     list.push({
-//       id: list.length + 1,
-//       name: TaskName,
-//       description: TaskDescription,
-//       status: TaskStatus,
-//     });
-//     localStorage.getItem(list)
-//       ? console.log("in memory")
-//       : localStorage.setItem("list", JSON.stringify(list));
-
-//     console.log("entry successful");
-//   }
-
-//   display();
-// };
-// clearTasks = () => {
-//   localStorage.length=0;
-//   console.log(localStorage);
-//   resetTasks();
-// };
-
-// editTasks = () => {
-//   alert("alert");
-// };
-
-// reviewTask = (caller) => {
-//   // use search function to select task
-//   let target = caller.id;
-//   console.log("area element id = " + target);
-// };
-
-// deleteItem = () => {
-//   let rawText = document.querySelector(".searchItem").textContent;
-//   let id = JSON.parse(rawText.split(" ").slice(10, 11)) - 1;
-//   console.log(id);
-//   localStorage.removeItem(list[id]);
-//   console.log(list);
-//   // window.location.reload();
-//   console.log("removal=complete");
-// };
+    RetrievedTaskArray.pop();
+    let clear = RetrievedTaskArray;
+    localStorage.setItem("list", JSON.stringify(clear));
+    window.location.reload();
+  }
+};
